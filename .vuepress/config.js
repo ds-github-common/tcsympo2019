48f5e7bc407a966c5937547261e6e1a13a1b0ad9
hs-yonekura <y-yonekura@science.co.jp>
2019-06-04 14:35:06 +0900

    firebase deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -25,35 +25,41 @@ module.exports = {
     },
     '@vuepress/nprogress': true
   },
-  title: 'Webマニュアル30分マニュアル',
-  description: 'MarkdownにJAMstackを添えて',
-  head: [
-    ['link', { href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css', rel: 'stylesheet' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/yakuhanjp@3.0.0/dist/css/yakuhanrp.min.css' }],
-    ['link', { rel: 'stylesheet', href: `https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700&amp;subset=japanese` }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['link', { rel: 'icon', href: 'image/favicon-512x512.png' }],
-    ['link', { rel: 'apple-touch-icon', href: 'image/favicon-192x192.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)', href: 'image/apple-launch-1242x2688.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-828x1792.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)', href: 'image/apple-launch-1125x2436.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)', href: 'image/apple-launch-1242x2208.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-750x1334.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-2048x2732.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-1668x2388.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-1668x2224.png' }],
-    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-1536x2048.png' }],
-    ['meta', { name: 'theme-color' , content: '#317EFB' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    [
-      'link',
-      {
-        rel: 'stylesheet',
-        href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css',
-        crossorigin: 'anonymous'
-      }
-    ]
-  ],
+  base: '/',
+  locales: {
+    '/': {
+      lang: 'ja',
+      title: 'Webマニュアル30分マニュアル',
+      description: 'MarkdownにJAMstackを添えて',
+      head: [
+        ['link', { href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css', rel: 'stylesheet' }],
+        ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/yakuhanjp@3.0.0/dist/css/yakuhanrp.min.css' }],
+        ['link', { rel: 'stylesheet', href: `https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700&amp;subset=japanese` }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['link', { rel: 'icon', href: 'image/favicon-512x512.png' }],
+        ['link', { rel: 'apple-touch-icon', href: 'image/favicon-192x192.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)', href: 'image/apple-launch-1242x2688.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-828x1792.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)', href: 'image/apple-launch-1125x2436.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)', href: 'image/apple-launch-1242x2208.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-750x1334.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-2048x2732.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-1668x2388.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-1668x2224.png' }],
+        ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)', href: 'image/apple-launch-1536x2048.png' }],
+        ['meta', { name: 'theme-color' , content: '#317EFB' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        [
+          'link',
+          {
+            rel: 'stylesheet',
+            href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css',
+            crossorigin: 'anonymous'
+          }
+        ]
+      ]
+    }
+  },
   themeConfig: {
     algolia: {
       appId: 'X9IZBSNGXM',
