7b7ed171ab86443077f8a520aa0e5d65827db3cb
hs-yonekura <y-yonekura@science.co.jp>
2019-06-05 23:23:22 +0900

    firebase deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -33,6 +33,8 @@ module.exports = {
   title: 'Webマニュアル30分マニュアル',
   description: 'MarkdownにJAMstackを添えて',
   head: [
+    ['link', { href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css', rel: 'stylesheet' }],
+    ['link', { rel: 'stylesheet', href: `https://fonts.googleapis.com/css?family=Roboto:300,400,700` }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['link', { rel: 'icon', href: 'image/favicon-512x512.png' }],
     ['link', { rel: 'apple-touch-icon', href: 'image/favicon-192x192.png' }],
