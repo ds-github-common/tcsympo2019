e593c73528bf1ca989efcac95c3457ad0ce6bd23
hs-yonekura <y-yonekura@science.co.jp>
2019-06-09 17:36:55 +0900

    fix

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -34,8 +34,7 @@ module.exports = {
   title: 'Webマニュアル30分マニュアル',
   description: 'MarkdownにJAMstackを添えて',
   head: [
-    ['link', { href: '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', rel: 'preload', as: 'style' }],
-      ['link', { href: '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/fonts/fontawesome-webfont.woff2?v=4.3.0', rel: 'preload', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
+    ['link', { href: '/fontawesome/css/all.min.css', rel: 'stylesheet' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['link', { rel: 'icon', href: 'image/favicon-512x512.png' }],
     ['link', { rel: 'apple-touch-icon', href: 'image/favicon-192x192.png' }],
