55b0578b835d382d2bd24d989ed8a8f865f5dbae
hs-yonekura <y-yonekura@science.co.jp>
2019-09-10 13:26:05 +0900

    fixed

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -41,7 +41,7 @@ module.exports = {
 			description: $page => $page.frontmatter.description,
 			author: (_, $site) => $site.themeConfig.author,
 			tags: $page => $page.frontmatter.tags,
-			twitterCard: _ => 'summary.png',
+      twitterCard: _ => 'image/summary.png',
 			type: $page => ['articles', 'posts', 'blog'].some(folder => $page.regularPath.startsWith('/' + folder)) ? 'article' : 'website',
 			url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
 			image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image),
